00469663e72f939be2c55a338f93bcd88bfb2d32
skylerwu <54692428+skylerwu@users.noreply.github.com>
2020-05-20 23:26:53 -0700

    Initial Save

---
+++ src/main/java/com/amazonaws/samples/CreateTable.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
+
+
+package com.amazonaws.samples;
+
+import java.util.Arrays;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+
+public class CreateTable {
+
+    public static void main(String[] args) throws Exception {
+    	
+        ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+        try {
+            credentialsProvider.getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (/Users/johnmortensen/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+        	.withCredentials(credentialsProvider)
+            .withRegion("us-west-1")
+            .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        String tableName = "Movies";
+
+        try {
+            System.out.println("Attempting to create table; please wait...");
+            Table table = dynamoDB.createTable(tableName,
+                Arrays.asList(new KeySchemaElement("year", KeyType.HASH), // Partition
+                                                                          // key
+                    new KeySchemaElement("title", KeyType.RANGE)), // Sort key
+                Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N),
+                    new AttributeDefinition("title", ScalarAttributeType.S)),
+                new ProvisionedThroughput(10L, 10L));
+            table.waitForActive();
+            System.out.println("Success.  Table status: " + table.getDescription().getTableStatus());
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to create table: ");
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
---
+++ src/main/java/com/amazonaws/samples/FoodQuery.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
+
+
+
+package com.amazonaws.samples;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+
+public class FoodQuery {
+
+    public static void main(String[] args) throws Exception {
+
+    	ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+        try {
+            credentialsProvider.getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (/Users/johnmortensen/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+        	.withCredentials(credentialsProvider)
+            .withRegion("us-west-1")
+            .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("Foods");
+
+        HashMap<String, Object> valueMap = new HashMap<String, Object>();
+        valueMap.put(":restaurant", "Pizza Hut");
+
+        QuerySpec querySpec = new QuerySpec().withKeyConditionExpression("Restaurant = :restaurant")
+            .withValueMap(valueMap);
+
+        ItemCollection<QueryOutcome> items = null;
+        Iterator<Item> iterator = null;
+        Item item = null;
+
+        try {
+            System.out.println("Menu Items from Pizza Hut");
+            items = table.query(querySpec);
+
+            iterator = items.iterator();
+            while (iterator.hasNext()) {
+                item = iterator.next();
+                System.out.println(item.getString("Restaurant") + ": " + item.getString("Food"));
+            }
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to query menu items from Pizza Hut");
+            System.err.println(e.getMessage());
+        }
+
+    }
+}
